b23f8e2200ed4adae8fbd2e1040268018290f8e5
elysewarren <eawdesigns@aol.com>
2017-03-31 13:29:05 -0400

    Final Commit.

--- app/src/main/java/com/elysewarren/inclassassignment07_elysew/MainActivity.java
+++ app/src/main/java/com/elysewarren/inclassassignment07_elysew/MainActivity.java
@@ -3,29 +3,43 @@ package com.elysewarren.inclassassignment07_elysew;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseDatabase database = FirebaseDatabase.getInstance();
-    private DatabaseReference dogRef = database.getReference("dog");
+    private DatabaseReference titleRef = database.getReference("title");
     private DatabaseReference dogsRef = database.getReference("dogs");
 
-
+    private EditText displayTitle;
+    private EditText displayBreed;
+    private EditText displayAge;
+    private CheckBox checkBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayTitle = (EditText) findViewById(R.id.title_view);
+        displayBreed = (EditText) findViewById(R.id.dog_breed);
+        displayAge = (EditText) findViewById(R.id.dog_age);
+        checkBox = (CheckBox) findViewById(R.id.allergy_type);
     }
 
     public void set(View view) {
-        dogRef.setValue(new Dog("Poodle", 10, false));
+        titleRef.setValue(displayTitle.getText().toString());
     }
 
     public void add(View view) {
-        dogsRef.push().setValue(new Dog("Yorkie", 11, true));
+        String dogType = displayBreed.getText().toString();
+        int dogAge = Integer.parseInt(displayAge.getText().toString());
+        boolean isAllergic = checkBox.isChecked();
+
+        Dog yorkie = new Dog(dogType, dogAge, isAllergic);
+        dogsRef.push().setValue(yorkie);
     }
 
 }
